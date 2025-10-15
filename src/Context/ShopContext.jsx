caedd030da67796ca4f3fe37d0aab998237206a8
Afsana Noor Keya <webafsanakeya@gmail.com>
2025-10-16 02:59:54 +0600

    first commit

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,26 +1,73 @@
-import React, { createContext, useState } from "react";
-import all_products from "../Components/Assets/all_product.js";
+import React, { createContext, useEffect, useState } from "react";
+
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
   let cart = {};
-  for (let index = 0; index < all_products.length + 1; index++) {
+  for (let index = 0; index <  300 + 1; index++) {
     cart[index] = 0;
   }
   return cart;
 };
 
 const ShopContextProvider = (props) => {
+  const [all_products, setAll_Products] = useState([]);
   const [cartItems, setCartItems] = useState(getDefaultCart());
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/allProducts')
+    .then((response)=>response.json())
+    .then((data)=> setAll_Products(data))
+    if(localStorage.getItem('auth-token')){
+      fetch('http://localhost:5000/getCart', {
+        method:'POST',
+        headers:{
+          Accept:'application/form-data',
+          'auth-token':`${localStorage.getItem('auth-token')}`,
+          'Content-Type':'application/json',
+        },
+        body:"",
+      }).then((response)=>response.json())
+      .then((data)=>setCartItems(data));
+    }
+
+  },[])
 
   const addToCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-    console.log(cartItems);
+    if(localStorage.getItem('auth-token')){
+      fetch('http://localhost:5000/addToCart', {
+        method:'POST',
+        headers:{
+          Accept:'application/form-data',
+          'auth-token':`${localStorage.getItem('auth-token')}`,
+          'Content-Type':'application/json',
+        },
+        body: JSON.stringify({"itemId":itemId})
+      })
+      .then((response)=>response.json())
+      .then((data)=>console.log(data))
+    }
+  
   };
 
   const removeFromCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    if(localStorage.getItem('auth-token')){
+        fetch('http://localhost:5000/removeFromCart', {
+        method:'POST',
+        headers:{
+          Accept:'application/form-data',
+          'auth-token':`${localStorage.getItem('auth-token')}`,
+          'Content-Type':'application/json',
+        },
+        body: JSON.stringify({"itemId":itemId})
+      })
+      .then((response)=>response.json())
+      .then((data)=>console.log(data))
+
+    }
   };
 
   const getTotalCartAmount = () => {
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,25 +1,94 @@
-import React from 'react'
-import './CSS/LoginSignup.css'
+import React, { useState } from "react";
+import "./CSS/LoginSignup.css";
 
 const LoginSignup = () => {
+  const [state, setState] = useState("Login");
+
+  const [formData, setFormData] = useState({
+    username:"",
+    password:"",
+    email:""
+  })
+
+  const changeHandler = (e)=>{
+    setFormData({...formData,[e.target.name]:e.target.value})
+
+  }
+
+  const login = async()=>{
+    console.log("Login function Executed", formData);
+        let responseData;
+    await fetch('http://localhost:5000/login', {
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify(formData),
+    }).then((response)=> response.json())
+    .then((data)=> responseData=data)
+    if(responseData.success){
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace("/");
+    }
+    else{
+      alert(responseData.errors)
+    }
+
+  }
+
+   const signup = async()=>{
+    console.log("Sign Up function executed", formData);
+    let responseData;
+    await fetch('http://localhost:5000/signup', {
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify(formData),
+    }).then((response)=> response.json())
+    .then((data)=> responseData=data)
+    if(responseData.success){
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace("/");
+    }
+    else{
+      alert(responseData.errors)
+    }
+    
+  }
   return (
-    <div className='loginSignup'>
+    <div className="loginSignup">
       <div className="loginSignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginSignup-fields">
-          <input type="text" placeholder='Your name' />
-          <input type="email" placeholder='Your Email' />
-          <input type="password" placeholder='Password' />
+          {state === "Sign Up" ? (
+            <input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder="Your name" />
+          ) : (
+            <></>
+          )}
+          <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder="Your Email" />
+          <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder="Password" />
         </div>
-        <button>Continue</button>
-        <p className="loginSignup-login">Already have an account?<span>Login here</span></p>
+        <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
+        {state === "Sign Up" ? (
+          <p className="loginSignup-login">
+            Already have an account?<span onClick={()=>{setState("Login")}}>Login here</span>
+          </p>
+        ) : (
+          <p className="loginSignup-login">
+            Create an account?<span onClick={()=>{setState("Sign Up")}}>Click here</span>
+          </p>
+        )}
+
         <div className="loginSignup-agree">
-          <input type="checkbox" name='' id='' />
+          <input type="checkbox" name="" id="" />
           <p>By continuing, i agree to the terms of use & privacy policy.</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
